f970382225661f9b8391c9f456197cf92f0fb9ae
Dikendra Baduwal <141381878+Dikendra-123@users.noreply.github.com>
2025-08-08 20:00:14 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Dikendra Baduwal",
+    author_email="dikendrabaduwal98@gmail.com",
+    packages=find_packages(),
+    install_requires=[]   
+    
+
+
+
+
+
+
+)
---
+++ src/prompt.py
@@ -0,0 +1,10 @@
+system_prompt=(
+    "you are an Medical Chatbot assistant for question-answer task."
+    "the question if you don't know the answer,say that you donot know the answer"
+    "use five sentences maximum and keep the answer consise and dinot repeat the same answer again and again and not even the same sentence."
+    "answer concise and precise." 
+    "\n\n"
+    "{context}"
+
+)
+
